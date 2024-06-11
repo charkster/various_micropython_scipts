7ab10d59ed1c0f0fd2df1bc091a42be5dd02d0b4
charkster <31560568+charkster@users.noreply.github.com>
2024-06-11 15:13:05 -0700

    Update esp32c3_wifi_email_from _motion_detect.py

--- thonny/esp32c3_wifi_email_from _motion_detect.py
+++ thonny/esp32c3_wifi_email_from _motion_detect.py
@@ -9,8 +9,8 @@ p21 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 def connect_to_wifi():
     # Your network credentials
-    ssid = '0024A515BC7E'
-    password = 'x3p08mh52h257'
+    ssid = 'ssid_123'
+    password = 'PASSWORD'
     #Connect to Wi-Fi
     wlan = network.WLAN(network.STA_IF)
     time.sleep_ms(1000)
